f00c601107c38a92f661c799f16c1f8beded93a6
Erwan MAIGNE MONTAMAT <emaignemontamat@ic-hn-services.center>
2024-02-02 14:15:22 +0100

    updated route for new game management

--- frontgraphqlpoc/src/app/app.routes.ts
+++ frontgraphqlpoc/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { ReviewComponent } from './review/review.component';
 import { GamesListComponent } from './GamesComponents/games-list/games-list.component';
 import { AddGameComponent } from './GamesComponents/add-game/add-game.component';
+import { GameCenterComponent } from './GamesComponents/game-center/game-center.component';
 
 export const routes: Routes = [
   {
     path: 'reviews',
     component: ReviewComponent,
   },
-  { path: 'games', component: GamesListComponent },
+  { path: 'games', component: GameCenterComponent },
   { path: 'add-game', component: AddGameComponent },
 ];
